e088eee791376bc069eaa6c79f1691babde96d75
Kevin Jacob <kevinjacob2001@gmail.com>
2020-06-22 00:12:03 +0530

    project completed without proper ui

--- counter.js
+++ counter.js
@@ -1,25 +1,39 @@
 
 
-let val=document.getElementById("field").innerHTML;
+let val = document.getElementById("field").innerHTML;
 
 
-
-
-
-
-
-
-document.querySelector("#Increment").addEventListener("click",function(){
-   val++;
-   document.querySelector("#field").textContent=val;
+document.querySelector("#Increment").addEventListener("click", function () {
+    val++;
+    document.querySelector("#field").textContent = val;
 })
 
-document.querySelector("#Decrement").addEventListener("click",function(){
+document.querySelector("#Decrement").addEventListener("click", function () {
     val--;
-    document.querySelector("#field").textContent=val;
- })
+    document.querySelector("#field").textContent = val;
+})
 
- document.querySelector("#reset").addEventListener("click",function(){
-     val=0;
-     document.querySelector("#field").textContent=val;
- })
+document.querySelector("#reset").addEventListener("click", function () {
+    val = 0;
+    document.querySelector("#field").textContent = val;
+})
+
+document.querySelector("#start").addEventListener("click", function () {
+
+    var x = setInterval(function () {
+        document.querySelector("#field").textContent = val;
+        val--;
+        if (val == -1) {
+            console.log("winner")
+            clearInterval(x);
+        }
+    }, 500)
+}
+);
+
+document.querySelector("#reset").addEventListener("click",
+    function () {
+        val = 25;
+        document.querySelector("#field").textContent = val;
+    })
+
